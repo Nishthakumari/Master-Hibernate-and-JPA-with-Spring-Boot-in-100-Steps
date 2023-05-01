fd0d498598f0a8052ba797c2ebb37fdcdc9990c6
Nishthakumari <knishtha0@gmail.com>
2023-05-01 11:18:08 +0530

    Step 10 - Quick introduction to JPA

--- src/main/java/com/in28minutes/database/databasedemo/jdbc/PersonJdbcDAO.java
+++ src/main/java/com/in28minutes/database/databasedemo/jdbc/PersonJdbcDAO.java
@@ -2,7 +2,6 @@ package com.in28minutes.database.databasedemo.jdbc;
 
 import com.in28minutes.database.databasedemo.entity.Person;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
