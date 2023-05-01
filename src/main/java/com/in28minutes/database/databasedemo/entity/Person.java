692c585bbb05837f58ab65fbc87848d1a2587997
Nishthakumari <knishtha0@gmail.com>
2023-05-01 11:23:25 +0530

    Step 11 - Defining Person Entity

--- src/main/java/com/in28minutes/database/databasedemo/entity/Person.java
+++ src/main/java/com/in28minutes/database/databasedemo/entity/Person.java
@@ -1,8 +1,16 @@
 package com.in28minutes.database.databasedemo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
 import java.util.Date;
 
+@Entity
 public class Person {
+
+    @Id
+    @GeneratedValue
     private int id;
     private String name;
     private String location;
@@ -14,6 +22,12 @@ public class Person {
 
     public Person(int id, String name, String location, Date birthDate) {
         this.id = id;
+        this.name = name;
+        this.location = location;
+        this.birthDate = birthDate;
+    }
+
+    public Person( String name, String location, Date birthDate) {
         this.name = name;
         this.location = location;
         this.birthDate = birthDate;
